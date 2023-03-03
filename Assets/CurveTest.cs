83484a3afe77c3a7f101bbfe30120501c0467473
Nail Sharipov <nailxsharipov@gmail.com>
2023-03-03 11:33:35 +0300

    update test

---
+++ Assets/CurveTest.cs
@@ -0,0 +1,54 @@
+using iShape.Spline;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CurveTest : MonoBehaviour {
+
+    public Anchor[] anchors;
+    public bool isClosed;
+    public bool isContour;
+    public float step = 0.5f;
+    
+    private void OnDrawGizmos() {
+        if (anchors.Length >= 3) {
+            float3 pos = this.transform.position;
+            NativeArray<float2> points;
+            NativeArray<Anchor> nAnchors = new NativeArray<Anchor>(anchors, Allocator.Temp);
+            if (isContour) {
+                var contour = new Contour(nAnchors, isClosed);
+                points = contour.GetPoints(step, new float2(pos.x, pos.y), Allocator.Temp);
+            } else {
+                var curve = new Curve(nAnchors, isClosed);
+                points = curve.GetPoints(step, new float2(pos.x, pos.y), Allocator.Temp);
+            }
+
+            nAnchors.Dispose();
+            
+            int n = points.Length;
+
+            int start;
+            int last;
+            if (isClosed) {
+                start = 0;
+                last = n - 1;
+            } else {
+                start = 1;
+                last = 0;
+            }
+
+            Gizmos.color = Color.magenta;
+            Vector3 a = new float3(points[last], 0);
+
+            for (int i = start; i < n; ++i) {
+                Vector3 b = new float3(points[i], 0);
+                Gizmos.DrawLine(a, b);
+                a = b;
+            }
+
+            points.Dispose();
+        }
+
+
+    }
+}
--- Assets/QuarticTest.cs
+++ Assets/QuarticTest.cs
@@ -9,18 +9,7 @@ public class QuarticTest : MonoBehaviour {
     
     private void OnDrawGizmos() {
         float3 pos = this.transform.position;
-        
-        curve.
-        
-        var spline = new QuarticSpline(A, B, AB);
-        
-        Gizmos.color = color;
-        Vector3 a = new float3(spline.Point(0), 0) + pos;
-        int n = 20;
-        for (int i = 1; i <= n; ++i) {
-            Vector3 b = new float3(spline.Point((float)i / n), 0) + pos;
-            Gizmos.DrawLine(a, b);
-            a = b;
-        }
+
+
     }
 }
